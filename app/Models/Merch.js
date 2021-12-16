33d14d9885dcf8487f56d86c68978f8f05d4dec5
SamanthaDison <samnicdis@gmail.com>
2021-12-16 16:25:47 -0700

    need to add functionality

--- app/Models/Merch.js
+++ app/Models/Merch.js
@@ -11,14 +11,13 @@ export class merchItem {
 
 
     get Template() {
-        return `<div class="col-6 d-flex">
+        return `<div class="col-6 my-2 p-2  d-flex ">
             <div class="bg-white justify-content-center rounded shadow p-3 ">
               <img class="img-fluid merch-img pb-1"
                 src="${this.imgUrl}"
                 alt="">
               <div class="d-flex justify-content-evenly">
-                <p>${this.name}</p>
-                <p>$${this.price}</p>
+                <p>${this.name} - $${this.price}</p>
               </div>
             </div>
           </div>`
